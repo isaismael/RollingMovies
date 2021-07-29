c849300529b859b865cd4aea4352296ed6734f55
TomasACV <tomasacv21407@gmail.com>
2021-07-29 15:34:16 -0300

    trabajmos en diseño y funcionalidad de registro y login

---
+++ js/login.js
@@ -0,0 +1,8 @@
+//Para login-------------------------------------------------
+// function validar() {
+//     let inputEmail = document.querySelector("#email");
+//     let inputPassword = document.querySelector("#password");
+
+//     let user = usuarios.find(function(user) {
+//                 return user.email === inputEmail.value;
+//             }
