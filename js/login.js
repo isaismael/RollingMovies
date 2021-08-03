e5101cf02e6759006f37f2d810e4b27d92595ac2
TomasACV <tomasacv21407@gmail.com>
2021-08-03 13:11:36 -0300

    añadimos cambios

--- js/login.js
+++ js/login.js
@@ -1,37 +1,68 @@
+// let arrayusuarios = [];
+// let form = document.getElementById("form2");
+// let email = document.getElementById("email2");
+// let password = document.getElementById("password2");
+
+// let users = JSON.parse(localStorage.getItem('users')) || [];
+// console.log(users);
+
+// class User {
+//     constructor(user, pass) {
+//         this.user = user;
+//         this.pass = pass;
+//     }
+// }
+
+// form1.addEventListener('submit', function(event) {
+//     event.preventDefault();
+
+//     if (users.length) {
+//         let index;
+//         console.log(email.value);
+//         users.filter((el, i) => {
+//             if (el.user == email.value) {
+//                 index = i;
+//             }
+//         });
+//         if (index !== undefined) {
+//             if (password.value == users[index].pass) {
+//                 localStorage.setItem("user", JSON.stringify(users[index]));
+//                 window.location.replace("/index.html");
+//             }
+//         }
+//     } else {
+//         alert("correo o contraseña incorrectos");
+//     };
+
+// });
 let arrayusuarios = [];
 let form = document.getElementById("form2");
 let email = document.getElementById("email2");
 let password = document.getElementById("password2");
-
-let users = JSON.parse(localStorage.getItem('users')) || [];
+let users = JSON.parse(localStorage.getItem("users")) || [];
 console.log(users);
-
 class User {
     constructor(user, pass) {
         this.user = user;
         this.pass = pass;
     }
 }
-
-form1.addEventListener('submit', function(event) {
+form.addEventListener("submit", function(event) {
     event.preventDefault();
-
-    if (users.length) {
+    if (users.length > 0) {
         let index;
         console.log(email.value);
-        users.filter((el, i) => {
-            if (el.user == email.value) {
-                index = i;
-            }
+        index = users.findIndex((item) => {
+            return item.email === email.value;
         });
-        if (index !== undefined) {
-            if (password.value == users[index].pass) {
+        console.log(index);
+        if (index !== -1) {
+            if (password.value == users[index].password) {
                 localStorage.setItem("user", JSON.stringify(users[index]));
-                window.location.href = "./index.html"
+                window.location.replace("../index.html");
             }
         }
     } else {
         alert("correo o contraseña incorrectos");
-    };
-
+    }
 });
--- js/registro.js
+++ js/registro.js
@@ -17,3 +17,12 @@ form1.addEventListener('submit', function(e) {
     oldUser.push(user);
     localStorage.setItem('users', JSON.stringify(oldUser));
 });
+
+<
+script type = "text/javascript"
+src = "https://cdn.jsdelivr.net/npm/emailjs-com@3/dist/email.min.js" > < /script> <
+    script type = "text/javascript" >
+    (function() {
+        emailjs.init("user_Ez93saRR2IKOH8pI6snTx");
+    })(); <
+/script>
